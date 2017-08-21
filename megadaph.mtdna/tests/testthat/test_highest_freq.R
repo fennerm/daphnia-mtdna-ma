797d1cdb4a202b2e986c8e5882c6198fb7b5d294
Fenner Macrae <fennermacrae@gmail.com>
2017-08-20 17:01:14 -0700

    Temporarily removed unfinished test

--- megadaph.mtdna/tests/testthat/test_highest_freq.R
+++ megadaph.mtdna/tests/testthat/test_highest_freq.R
@@ -7,7 +7,8 @@ us <- readRDS("../dat/unstranded_pile_row.Rds")
 s_maj <- highest_freq(s)
 us_maj <- highest_freq(us)
 
-test_that("Output correct", {
-    expect_equal(12, length(s_sub))
-    expect_equal(6, length(us_sub))
-})
+## UNFINISHED
+# test_that("Output correct", {
+#     expect_equal(12, length(s_sub))
+#     expect_equal(6, length(us_sub))
+# })
