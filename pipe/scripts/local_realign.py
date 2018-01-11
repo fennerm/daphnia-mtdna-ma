3fc9d3d7ade5c80081f3d18f6b381a62023226d7
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-01-11 11:02:34 -0800

    Fixed bug in local realignment script

--- pipe/scripts/local_realign.py
+++ pipe/scripts/local_realign.py
@@ -11,4 +11,4 @@ shell("gatk -T IndelRealigner -R {snakemake.input.ref} -I "
       "{snakemake.params.max_realignment_reads} --maxPositionalMoveAllowed "
       "{snakemake.params.max_move} --maxReadsForConsensuses "
       "{snakemake.params.max_cons_reads} --log_to_file {snakemake.log} "
-      "--maxConsensuses {snakemake.params.max_cons})
+      "--maxConsensuses {snakemake.params.max_cons}")
