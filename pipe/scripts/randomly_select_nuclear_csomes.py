e56f78c50d447b84370053ffa688dcf307a00fc6
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-01-18 15:35:43 -0800

    Added random selection of nuclear chromosomes to pipeline

---
+++ pipe/scripts/randomly_select_nuclear_csomes.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import random
+
+from plumbum import local
+from plumbum.cmd import list_nonempty_csomes
+
+
+def partition_by_species(samples):
+    """Split the list of input .bam files by their species"""
+    pulex = []
+    magna = []
+    for sample in samples:
+        if sample.name.startswith(["F", "G", "I"]):
+            magna.append(sample)
+        elif sample.name.startswith(["L", "TCO"]):
+            pulex.append(sample)
+    return pulex, magna
+
+
+samples = [local.path(sample) for sample in snakemake.input]
+species = ("pulex", "magna")
+species_split = partition_by_species(samples)
+for spp, subsplit in zip(species, species_split):
+    # Get all csomes with at least one alignment
+    csomes = [list_nonempty_csomes(sample) for sample in species]
+
+    # Get csomes which have at least one alignment in all samples
+    shared_csomes = set(csomes[0]).intersection(*csomes)
+
+    # Randomly select some chromosomes
+    rand_csomes = random.sample(shared_csomes, snakemake.params.n)
+
+    # Convert to string and write to output file
+    shared_csomes = '\n'.join(shared_csomes)
+    outfile = snakemake.output[spp]
+    with open(outfile) as out:
+        out.write(shared_csomes)
