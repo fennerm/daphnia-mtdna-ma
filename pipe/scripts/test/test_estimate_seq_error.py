c87bdb93014473c30cd01da29a6de7254abe9eff
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-06-01 16:52:37 -0700

    Remove old files

--- pipe/scripts/test/test_estimate_seq_error.py
+++
@@ -1,9 +0,0 @@
-import os
-from fmbiopy.fmtest import *
-
-def test_estimate_seq_error_produces_valid_output(dat, sandbox):
-    bam = str(dat['tiny']['bam'][0])
-    csv = str(sandbox / "seq_error.csv")
-    os.system('samtools index ' + bam)
-    os.system(' '.join(["./estimate_seq_error.R", bam, csv]))
-    assert os.path.isfile(csv)
